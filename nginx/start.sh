b29239bf12b9965c1d5a5774977dbc61b09daaf3
dmitrii <mikrolux@gmail.com>
2025-04-30 17:34:18 +0700

    .

--- nginx/start.sh
+++ nginx/start.sh
@@ -5,8 +5,8 @@ cp ./nginx-init.conf /etc/nginx/nginx.conf
 echo "Запуск nginx с базовой конфигурацией..."
 nginx
 
-echo "Ожидание 5 секунд перед запуском certbot..."
-sleep 5
+echo "Ожидание 30 секунд перед запуском certbot..."
+sleep 30
 
 echo "Запуск certbot..."
 certbot --nginx -d 217-1141197.hopto.org -d www.217-1141197.hopto.org --non-interactive --agree-tos -m mikrolux@gmail.com
@@ -17,15 +17,12 @@ chmod 0644 /etc/cron.d/certbot-renew
 crontab /etc/cron.d/certbot-renew
 service cron start
 
-# Здесь должна быть загрузка финальной конфигурации, а не копирование файла в себя
-echo "Перезапуск nginx с окончательной конфигурацией..."
-# Если у вас есть отдельная конфигурация, используйте её
-cp ./nginx.conf /etc/nginx/nginx.conf
-# Или используйте конфигурацию, модифицированную certbot
+# Вместо копирования конфигурации - перезагрузка!
+echo "Перезагрузка nginx с окончательной конфигурацией..."
 nginx -s reload
 
 echo "Запуск nginx в режиме foreground..."
-# Останавливаем текущий процесс nginx
+# Останавливаем текущий процесс nginx (это может быть лишним, если reload работает)
 nginx -s stop
 # Запускаем новый в foreground режиме
 exec nginx -g "daemon off;"
