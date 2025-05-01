e5ed8308aac88d7056004bd4952f21c3202bf25c
dmitrii <mikrolux@gmail.com>
2025-05-01 21:30:48 +0700

    ok

--- nginx/start.sh
+++ nginx/start.sh
@@ -14,10 +14,10 @@ echo "Установка основного SSL-конфига nginx..."
 cp /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
 
 echo "Перезапуск nginx..."
-nginx stop
+nginx -s reload || nginx
 
-# echo "Nginx запущен, переход в режим ожидания..."
-# tail -f /dev/null
+echo "Nginx запущен, переход в режим ожидания..."
+tail -f /dev/null
 
 # echo "Настройка автоматического продления сертификата..."
 # echo "0 12 * * * root certbot renew --quiet && nginx -s reload" > /etc/cron.d/certbot-renew
@@ -25,4 +25,4 @@ nginx stop
 # service cron start
 
 # echo "Запуск nginx в foreground..."
-exec nginx -g "daemon off;"
+# exec nginx -g "daemon off;"
