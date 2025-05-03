5ea4613390abca5cd40d960c4c206db0e8d33e0c
dmitrii <mikrolux@gmail.com>
2025-05-04 00:18:09 +0700

    test

--- nginx/start.sh
+++ nginx/start.sh
@@ -7,8 +7,12 @@ nginx
 
 curl -I http://localhost || echo "Локальный nginx не отвечает"
 
-echo "Получение SSL через certbot..."
-certbot --nginx -d 217-1141197.hopto.org --non-interactive --agree-tos -m mikrolux@gmail.com
+if [ -f "/etc/letsencrypt/live/217-1141197.hopto.org/fullchain.pem" ]; then
+  echo "Сертификат уже существует, пропускаем выдачу."
+else
+  echo "Получение SSL через certbot..."
+  certbot --nginx -d 217-1141197.hopto.org --non-interactive --agree-tos -m mikrolux@gmail.com
+fi
 
 echo "Установка основного SSL-конфига nginx..."
 cp /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
@@ -16,13 +20,10 @@ cp /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
 echo "Перезапуск nginx..."
 nginx -s reload || nginx
 
-echo "Nginx запущен, переход в режим ожидания..."
-tail -f /dev/null
+echo "Настройка автоматического продления сертификата..."
+echo "0 3 * * * root certbot renew --quiet && nginx -s reload" > /etc/cron.d/certbot-renew
+chmod 0644 /etc/cron.d/certbot-renew
+cron
 
-# echo "Настройка автоматического продления сертификата..."
-# echo "0 12 * * * root certbot renew --quiet && nginx -s reload" > /etc/cron.d/certbot-renew
-# chmod 0644 /etc/cron.d/certbot-renew
-# service cron start
-
-# echo "Запуск nginx в foreground..."
-# exec nginx -g "daemon off;"
+echo "Запуск nginx в foreground..."
+exec nginx -g "daemon off;"
